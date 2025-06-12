d1238b140239681a378431e55ed9c16c49bf78e3
Mauma9 <mauricioribeirodefrias@gmail.com>
2025-06-12 12:28:22 -0300

    atualizacao

---
+++ dia12/DynamicArray.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int tamanho = 0;
+int *idades = ((int*)calloc(idades, sizeof(int)));
+
+printf("Quantas idades? ");
+ scanf("%d", &tamanho);
+
+ if (idades == NULL) {
+ printf("Sem memória!\n");
+ return 1;
+ }
+
+ for (int i = 0; i < tamanho; i++) {
+ printf("Idade %d: ", i+1);
+ scanf("%d", &idades[i]);
+ }
+
+
+ printf("Voce digitou: ");
+ for (int i = 0; i < tamanho; i++) {
+ printf("%d ", idades[i]);
+ }
+
+ free(idades);
+ idades = NULL;
+return 0;
+}
